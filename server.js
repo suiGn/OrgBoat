37fcac15c190faa9c8a7d5858665843612ba0a17
Sui Gn <natsynapses@gmail.com>
2023-08-10 00:14:11 -0500

    push orgboat
    init orgboat

--- server.js
+++ server.js
@@ -4,16 +4,15 @@ var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 var fs = require('fs');
-
+const { serialize } = require('v8');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 //routes    
 app.get('/', function(req, res) {
-    res.send('Hello World');
+    res.send('Welcome to orgboat.me!');
 }
 );
-
 
 //start server
 var server = app.listen(3080, function() {
