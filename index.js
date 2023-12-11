1a089180f69d13dd591463ca356510b392103cda
Sui Gn <natsynapses@gmail.com>
2023-12-10 21:00:34 -0600

    assets
    -

---
+++ index.js
@@ -0,0 +1,39 @@
+//https://wa.me/+527291671525?text=I'm%20interested%20in%20your%20mezcal%20for%20sale
+//https://wa.me/+527291671525?text=I'm%20interested%20in%20your%20palo_azul%20for%20sale
+//https://wa.me/+527291671525?text=I'm%20interested%20in%20your%20this%20for%20sale
+
+
+//my whatsapp
+//my phone
+//my address
+//my instagram
+//my facebook
+//my twitter
+//my youtube
+//my tiktok
+//my snapchat
+//my pinterest
+//my linkedin
+//my github
+//my reddit
+//my telegram
+//my discord
+//my twitch
+//my medium
+//my tumblr
+//my soundcloud
+//my spotify
+//my bandcamp
+//my patreon
+//my paypal
+//my cashapp
+//my venmo
+//my onlyfans
+//my email - create class email to add email 
+
+let email = "   ";
+
+
+
+
+
